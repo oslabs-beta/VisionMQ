97d691ed33299625b15a5c86cf0ed7ce29e9720f
Jake Gibson <jakesgibson@gmail.com>
2023-12-11 11:42:06 -0500

    Installed vite/react flow boilerplate

---
+++ User-App/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import FlowChart from './components/FlowChart'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <FlowChart />
+    </>
+  )
+}
+
+export default App
---
+++ User-App/src/components/FlowChart.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+ 
+
+const FlowChart = (): JSX.Element => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+ 
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds: any) => addEdge(params, eds)),
+    [setEdges],
+  );
+ 
+  return (
+    <div style={{ width: '50vw', height: '50vh' , border: '1px solid black' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        {/* <Background variant="dots" gap={12} size={1} /> */}
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default FlowChart;
