cce1af46bc6f468d1e8c6730dc02885f02b01a47
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2023-12-20 10:48:43 -0500

    Fetches set

--- Prometheus/prometheus2.js
+++ Prometheus/prometheus2.js
@@ -1,32 +1,25 @@
-
-
-
 let fetcher = async() => {
 
-
-
-let published = await fetch('http://localhost:9090/api/v1/query?query=rabbitmq_queue_messages_published_total');
+let published = await fetch('http://localhost:9090/api/v1/query?query=rabbitmq_queue_messages');
 
       const published_object = await published.json()
-      const published_result = published_object.data.result[0].value[1]
+      const published_result = published_object.data.result
   
-
-      console.log('this is the amount published: ', published_result)
+      console.log('this is the amount in queues: ', published_result)
 
 let acknowlegded = await fetch('http://localhost:9090/api/v1/query?query=rabbitmq_queue_messages_acknowledged_total');
 
       const acknowledged_object = await acknowlegded.json()
       const acknowledged_result = acknowledged_object.data.result
 
-      console.log('this is the amount acknowledged: ', acknowledged_result)
-
-let delievered = await fetch('http://localhost:9090/api/v1/query?query=rabbitmq_queue_messages_delivered_total');
-
-  let delieverd_object = await delievered.json()
-  let delieverd_result = delieverd_object.data.result
-
-  console.log('this is the amount delieverd: ', delieverd_result)
-  
-}
+      console.log('this is the amount consumed: ', acknowledged_result)
 
 fetcher()
+
+}
+//rabbitmq_queue_messages_acknowledged_total
+
+
+//successful vs unsuccseful auth attempts
+      // rabbitmq_auth_attempts_succeeded_total
+      //rabbitmq_auth_attempts_failed_total
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # VisionMQ
+
+
+#Setting up Rabbit Firehose Tracer (used for tracking data)
+1. rabbitmq-plugins enable rabbitmq_tracing
+2. rabbitmqctl trace_on
+3. 
